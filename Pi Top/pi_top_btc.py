207027ee4de881a5b0a749f3811d77a5632ad871
Vala Zeinali <32586254+valazeinali@users.noreply.github.com>
2024-02-14 15:41:47 -0500

    using new api to get prices back to 2010.

--- Pi Top/pi_top_btc.py
+++ Pi Top/pi_top_btc.py
@@ -1,8 +1,63 @@
 import yfinance as yf
 import plotly.graph_objs as go
+import pandas as pd
+import requests
+from datetime import datetime, timedelta
 
 # Retrieve Bitcoin daily prices
-btc = yf.download('BTC-USD', start='2010-01-01', end='2100-02-13')
+#btc = yf.download('BTC-USD', start='2010-01-01', end='2100-02-13')
+
+# get data
+def get_all_time_historical_data(symbol, currency):
+    try:
+        # Initialize empty dictionary to store all-time historical data
+        all_time_data = {}
+
+        # Make request to CryptoCompare API for each year of data
+        current_year = datetime.now().year
+        start_year = 2010  # CryptoCompare data is available from 2010
+        for year in range(start_year, current_year + 1):
+            end_date = datetime(year + 1, 1, 1)
+            start_date = datetime(year, 1, 1)
+
+            # Convert dates to Unix timestamp format
+            end_date_unix = int(end_date.timestamp())
+            start_date_unix = int(start_date.timestamp())
+
+            # Make request to CryptoCompare API
+            url = 'https://min-api.cryptocompare.com/data/v2/histoday'
+            parameters = {
+                'fsym': symbol,
+                'tsym': currency,
+                'limit': (end_date - start_date).days,
+                'toTs': end_date_unix
+            }
+            response = requests.get(url, params=parameters)
+            data = response.json()
+
+            # Extract daily close prices
+            for entry in data['Data']['Data']:
+                date = datetime.utcfromtimestamp(entry['time']).strftime('%Y-%m-%d')
+                all_time_data[date] = entry['close']
+
+        return all_time_data
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
+
+# Get all-time historical data from CryptoCompare API
+symbol = 'BTC'
+currency = 'USD'
+bitcoin_all_time_data = get_all_time_historical_data(symbol, currency)
+
+if bitcoin_all_time_data:
+    # Create DataFrame from the dictionary
+    btc = pd.DataFrame(list(bitcoin_all_time_data.items()), columns=['Date', 'Close'])
+    btc['Date'] = pd.to_datetime(btc['Date'])  # Convert Date column to datetime type
+    btc = btc.set_index('Date')  # Set Date column as index
+    
+else:
+    print("Failed to retrieve data.")
 
 # Calculate moving averages
 btc['111DMA'] = btc['Close'].rolling(window=111).mean()
