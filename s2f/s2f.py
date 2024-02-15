6829591f36c64ad5be73c9146954a5a086debe84
DOGE2MOON <62063791+DOGE2MOON@users.noreply.github.com>
2024-02-15 10:41:45 -0500

    Add files via upload

--- s2f/s2f.py
+++ s2f/s2f.py
@@ -1,28 +1,30 @@
 import numpy as np
 import pandas as pd
+from pandas import json_normalize
 from datetime import datetime
 from datetime import date
-from coinmetrics.api_client import CoinMetricsClient
 import warnings
+import requests
+import json
 
 warnings.filterwarnings("ignore")
 
 def build_s2f(begin_timestamp, end_timestamp):
 	# construct the Bitcoin Stock-to-Flow Ratio using the CoinMetrics API
-	client = CoinMetricsClient()
 
 	# string of format %Y-%m-%d also accepted
 	if "datetime" in str(type(begin_timestamp)):
 		begin_timestamp = begin_timestamp.strftime("%Y-%m-%d")
 	if "datetime" in str(type(end_timestamp)):
 		end_timestamp = end_timestamp.strftime("%Y-%m-%d")
-
-	asset = "btc"
+	
 	# define the metrics to fetch
+	asset = "btc"
 	metrics = "SplyCur,IssContNtv,CapMrktCurUSD,PriceUSD,DiffMean"
+
 	# fetch the metrics
-	asset_data = client.get_asset_metrics(assets=asset, metrics=metrics, start_time=begin_timestamp, end_time=end_timestamp)
-	raw_data = pd.DataFrame(asset_data)
+	raw_data = requests.get(f'https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets={asset}&metrics={metrics}&start_time={begin_timestamp}&end_time={end_timestamp}&frequency=1d&page_size=10000&pretty=true').json()
+	raw_data = json_normalize(raw_data['data'])
 	# export raw data to csv
 	#raw_data.to_csv("CM_raw_data.csv", encoding='utf-8')
 	
@@ -50,10 +52,9 @@ def build_s2f(begin_timestamp, end_timestamp):
 	cleaned_data['fwd_issuance'] = np.multiply(cleaned_data['issuance'], 365)
 	cleaned_data['s2f'] = np.divide(cleaned_data[['tsupply']], cleaned_data[['fwd_issuance']])
 	cleaned_data['lns2f'] = np.log(cleaned_data[['s2f']])
-	# resample the data by week 
+	# resample the data by week
 	cleaned_data = cleaned_data.resample('W').mean()
 
 	return cleaned_data
-
 
 
