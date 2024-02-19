c44f0432f6ed4a7faf98566383b94b9e015039bb
NoturBusiness <104364732+NoturBusiness@users.noreply.github.com>
2024-02-19 04:13:34 -0800

    Create pi_cycle.py

---
+++ pi_cycle.py
@@ -0,0 +1,90 @@
+import plotly.graph_objs as go
+from data.get_historical_data import get_historical_data
+import numpy as np
+
+def get_pi_cycle_plot(symbol, currency):
+    data = get_historical_data(symbol, currency)
+    
+    # Calculate moving averages
+    data["111DMA"] = data["Close"].rolling(window=111).mean()
+    data["350DMA*2"] = data["Close"].rolling(window=350).mean() * 2
+    
+    # Calculate Pi Cycle Top Indicator
+    data["PiTop"] = data[["111DMA", "350DMA*2"]].max(axis=1)
+    
+    # Find where 111DMA > 350DMA * 2
+    data["Highlight"] = data["111DMA"] > data["350DMA*2"]
+    
+    # Create an interactive plotly graph
+    fig = go.Figure()
+    
+    # Add Bitcoin closing prices
+    fig.add_trace(
+        go.Scatter(
+            x=data.index,
+            y=data["Close"],
+            mode="lines",
+            name=symbol + " Close Price",
+            line=dict(color="orange"),
+        )
+    )
+    
+    # Add moving averages
+    fig.add_trace(
+        go.Scatter(
+            x=data.index,
+            y=data["111DMA"],
+            mode="lines",
+            name="111-day Moving Average",
+            line=dict(color="#FF97FF"),
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=data.index,
+            y=data["350DMA*2"],
+            mode="lines",
+            name="350-day Moving Average x2",
+            line=dict(color="CYAN"),
+        )
+    )
+    
+    # Add Pi Cycle Top Indicator
+    # fig.add_trace(go.Scatter(x=data.index, y=data['PiTop'], mode='lines', name='Pi Cycle Top Indicator', line=dict(color='red')))
+    
+    # Highlight areas where 111DMA > 350DMA * 2
+    highlighted_dates = data[data["Highlight"]].index
+    highlighted_prices = data[data["Highlight"]]["Close"]
+    fig.add_trace(
+        go.Scatter(
+            x=highlighted_dates,
+            y=highlighted_prices,
+            mode="markers",
+            marker=dict(color="red", size=6),
+            name="111DMA > (350DMA * 2)",
+        )
+    )
+    
+    # Set layout with black background
+    fig.update_layout(
+        title=symbol + "Daily Prices with Moving Averages and Pi Cycle Top Indicator",
+        xaxis=dict(title="Date"),
+        yaxis=dict(title="Price", type="log"),
+        plot_bgcolor="black",
+        paper_bgcolor="black",
+        font=dict(color="white"),
+    )
+
+
+    return fig
+
+
+
+if __name__ == "__main__":
+  # Get all-time historical data from CryptoCompare API
+  symbol = 'BTC'
+  currency = 'USD'
+  
+  fig = get_pi_cycle_plot(symbol, currency)
+  
+  fig.show()
