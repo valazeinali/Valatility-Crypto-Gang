56e822db7f50b5789f0539bc46e981ca78860ba4
NoturBusiness <104364732+NoturBusiness@users.noreply.github.com>
2024-02-19 02:45:53 -0800

    Update get_historical_data.py

--- data/get_historical_data.py
+++ data/get_historical_data.py
@@ -94,11 +94,86 @@ def get_historical_data(symbol, currency):
         df.to_parquet(filename)
         return df
 
+def get_coinmetrics_data(symbol):
+    
+    def get_data(symbol, last_date=None):
+        try:
+            # Get today's date
+            today = datetime.now().date()
+                
+            
+            yesterday = today - timedelta(days=1)
+                
+            # Save yesterday's date as end_date
+            end_date = yesterday.strftime('%Y-%m-%d')
 
+            if last_date is not None:
+                start_date = (last_date + timedelta(days=1)).strftime('%Y-%m-%d')
+            else:
+                start_date = datetime(2010, 1, 1).strftime('%Y-%m-%d')  # Start from the beginning if there's no last_date
+
+            metrics = "SplyCur,IssContNtv,CapMrktCurUSD,PriceUSD,DiffMean"
+            
+            # fetch the metrics
+            data = requests.get(
+                    f"https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets={symbol}&metrics={metrics}&start_time={start_date}&end_time={end_date}&frequency=1d&page_size=10000&pretty=true"
+            ).json()
+
+            data = json_normalize(data["data"])
+            
+            
+            data['time'] = pd.to_datetime(data['time'], utc=True)
+            
+            # Format the 'time' column as strings with the desired format
+            data['time'] = data['time'].dt.strftime('%Y-%m-%d')
+            
+            data = data.set_index('time')
+            
+            data.rename(columns={"SplyCur": "tsupply", "IssContNtv": "issuance"}, inplace=True)
+            data = data.astype({'tsupply': 'float', 'issuance': 'float', 'CapMrktCurUSD': 'float', 'DiffMean': 'float'})
+
+
+            return data
+        except Exception as e:
+            print("An error occurred:", e)
+            return None
+        
+    filename = f"{symbol}_S2F_data.parquet"
+    if os.path.exists(filename):
+        # Load the data from the parquet file
+        df = pd.read_parquet(filename)
+        
+        # Get the current date and time
+        time_now = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+
+        last_date = df.index[-1]
+
+        if  last_date == time_now:
+            return df
+        else:
+            # Get the new data
+            new_df = get_data(symbol, last_date)
+            
+            # Append the new data to the existing data
+            df = pd.concat([df, new_df]).sort_index()
+            df = df[~df.index.duplicated(keep='last')]
+            # Save the updated data to the parquet file
+            df.to_parquet(filename)
+            
+            return df
+    
+    else:
+        df = get_data(symbol)
+        
+        df = df[~df.index.duplicated(keep='last')]
+        
+        df.to_parquet(filename)
+        return df
+        
 #symbol = "BTC"
 #currency = "USD"
 
 #df = get_historical_data(symbol, currency)
-
+#df = get_coinmetrics_data(symbol)
 
 #display(df)    
