44944391458d74eff1e0d85d7835aa09e0d5cd17
NoturBusiness <104364732+NoturBusiness@users.noreply.github.com>
2024-02-26 02:31:16 -0800

    Update get_historical_data.py

--- data/get_historical_data.py
+++ data/get_historical_data.py
@@ -1,8 +1,8 @@
 from datetime import datetime, timedelta
 import pandas as pd
-from pandas import json_normalize
 import requests
 import os
+from pandas import json_normalize
 
 
 def get_historical_data(symbol, currency):
@@ -10,46 +10,36 @@ def get_historical_data(symbol, currency):
     def get_data(symbol, currency, last_date=None):
         try:
 
-            # Make request to CryptoCompare API for each year of data
-            current_year = datetime.now().year
-            
-
-            if last_date is not None:
-                start_date = last_date + timedelta(days=1)
-            else:
-                start_date = datetime(2010, 1, 1)  # Start from the beginning if there's no last_date
-
             all_time_data = []
-            
-            for year in range(start_date.year, current_year + 1):
-                end_date = datetime(year + 5, 1, 1)
-                start_date = datetime(year, 1, 1)
 
-                # Convert dates to Unix timestamp format
-                end_date_unix = int(end_date.timestamp())
-                start_date_unix = int(start_date.timestamp())
-
-                # Make request to CryptoCompare API
-                url = "https://min-api.cryptocompare.com/data/v2/histoday"
+            # Make request to CryptoCompare API
+            url = "https://min-api.cryptocompare.com/data/v2/histoday"
+            if last_date is None:
                 parameters = {
-                    "fsym": symbol,
-                    "tsym": currency,
-                    "limit": (end_date - start_date).days,
-                    "toTs": end_date_unix,
+                   "fsym": symbol,
+                   "tsym": currency,
+                   "allData": "true",
                 }
-                response = requests.get(url, params=parameters)
-                data = response.json()
+            else:
+                parameters = {
+                   "fsym": symbol,
+                   "tsym": currency,
+                   "toTs": int(datetime.now().timestamp()),
+                   "limit": 2000,
+                }
+            response = requests.get(url, params=parameters)
+            data = response.json()
 
 
-                # Extract daily close prices
-                for entry in data["Data"]["Data"]:
-                    date = datetime.utcfromtimestamp(entry["time"]).strftime("%Y-%m-%d")
-                    open_price = entry["open"]
-                    high_price = entry["high"]
-                    low_price = entry["low"]
-                    close_price = entry["close"]
-                    volume = entry["volumefrom"]
-                    all_time_data.append([date, open_price, high_price, low_price, close_price, volume])
+            # Extract daily close prices
+            for entry in data["Data"]["Data"]:
+                date = datetime.utcfromtimestamp(entry["time"]).strftime("%Y-%m-%d")
+                open_price = entry["open"]
+                high_price = entry["high"]
+                low_price = entry["low"]
+                close_price = entry["close"]
+                volume = entry["volumefrom"]
+                all_time_data.append([date, open_price, high_price, low_price, close_price, volume])
             return all_time_data
         except Exception as e:
             print("An historical data error occurred:", e)
@@ -98,8 +88,10 @@ def get_historical_data(symbol, currency):
         df["Date"] = pd.to_datetime(df["Date"], utc=True)  # Convert Date column to datetime type
         df = df.set_index("Date")  # Set Date column as index
         df = df[~df.index.duplicated(keep='last')]
+        df = df.loc[~(df == 0.0).all(axis=1)]
         df.to_parquet(filename)
         return df
+
 
 def get_coinmetrics_data(symbol):
     
@@ -176,6 +168,9 @@ def get_coinmetrics_data(symbol):
         
         df = df[~df.index.duplicated(keep='last')]
         
+        # Drop rows where all values are 0.0
+        df = df.loc[~(df == 0.0).all(axis=1)]
+
         df.to_parquet(filename)
         return df
         
