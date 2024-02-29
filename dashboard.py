f6e5cfc223bd5796133fd7bde5903b35338fcc6b
tdafonseca <t.zuquete.f@gmail.com>
2024-02-29 14:36:27 +0100

    Fixing cache from dashboard

--- dashboard.py
+++ dashboard.py
@@ -2,11 +2,11 @@ from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 from flask_caching import Cache
 
-from z_score import get_z_score_plot
-from seasonality_heatmap import get_seasonality_heatmap_plot
-from s2f import get_s2f_plot
-from pi_top import get_pi_top_plot
 from mcpc import get_mcpc_plot
+from pi_top import get_pi_top_plot
+from s2f import get_s2f_plot
+from seasonality_heatmap import get_seasonality_heatmap_plot
+from z_score import get_z_score_plot
 
 # Initialize the app
 app = Dash(__name__, assets_url_path="assets")
@@ -14,17 +14,6 @@ server = app.server
 app.title = "Valatility Crypto Dashboard"
 
 # Configure cache
-<<<<<<< HEAD
-cache = Cache(app.server, config={
-    'CACHE_TYPE': 'FileSystemCache',  # Simple caching strategy, suitable for development/testing
-    'CACHE_DIR': 'cache',
-<<<<<<< HEAD
-    'CACHE_DEFAULT_TIMEOUT': 3600  # 86,400 seconds == 24 hours
-=======
-    'CACHE_DEFAULT_TIMEOUT': 86400  # 86,400 seconds == 24 hours
->>>>>>> fd64180 (Update dashboard.py)
-})
-=======
 cache = Cache(
     app.server,
     config={
@@ -33,8 +22,6 @@ cache = Cache(
         "CACHE_DEFAULT_TIMEOUT": 86400,  # 86,400 seconds == 24 hours
     },
 )
->>>>>>> 5de1578 (Add shared pre-push hook script and formatted files)
-
 # Define your color scheme
 colors = {
     "main-background": "#111111",
@@ -167,29 +154,35 @@ def send_layout():
                     ),
                 ]
             ),
-<<<<<<< HEAD
-<<<<<<< HEAD
             html.Div(
                 children=[
-                    dcc.Graph(id="Bitcoin MCPC", style={"width": "100%", "vertical-align": "middle"}, config={'displaylogo': False, 'scrollZoom':True, 'modeBarButtonsToAdd': ['drawline', 'drawopenpath', 'drawclosedpath', 'drawcircle', 'drawrect', 'eraseshape']},
-                    figure={
-                        'layout': {
-                            'title': 'Bitcoin MCPC',
-                            'plot_bgcolor': 'rgba(17, 17, 17, 1)',
-                            'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-                            'font': {
-                                'color': 'white'
+                    dcc.Graph(
+                        id="Bitcoin MCPC",
+                        style={"width": "100%", "vertical-align": "middle"},
+                        config={
+                            "displaylogo": False,
+                            "scrollZoom": True,
+                            "modeBarButtonsToAdd": [
+                                "drawline",
+                                "drawopenpath",
+                                "drawclosedpath",
+                                "drawcircle",
+                                "drawrect",
+                                "eraseshape",
+                            ],
+                        },
+                        figure={
+                            "layout": {
+                                "title": "Bitcoin MCPC",
+                                "plot_bgcolor": "rgba(17, 17, 17, 1)",
+                                "paper_bgcolor": "rgba(0, 0, 0, 0)",
+                                "font": {"color": "white"},
                             }
-                        }
-                    }),
+                        },
+                    ),
+                    html.Button("Update Data", id="update-data", n_clicks=0),
                 ]
             ),
-=======
->>>>>>> fd64180 (Update dashboard.py)
-            html.Button('Update Data', id='update-data', n_clicks=0),
-=======
-            html.Button("Update Data", id="update-data", n_clicks=0),
->>>>>>> 5de1578 (Add shared pre-push hook script and formatted files)
         ],
     )
 
@@ -206,26 +199,14 @@ def get_cached_data(func, symbol="BTC", currency="USD"):
 
 # Callbacks to update each graph using the cached data
 @app.callback(
-<<<<<<< HEAD
-    [Output("Bitcoin Pi Cycle Top", "figure"),
-     Output("Bitcoin Seasonality Heatmap", "figure"),
-     Output("Bitcoin S2F", "figure"),
-<<<<<<< HEAD
-     Output("Bitcoin Z-Score", "figure"),
-     Output("Bitcoin MCPC", "figure")],
-=======
-     Output("Bitcoin Z-Score", "figure")],
->>>>>>> fd64180 (Update dashboard.py)
-    [Input("update-data", "n_clicks")]
-=======
     [
         Output("Bitcoin Pi Cycle Top", "figure"),
         Output("Bitcoin Seasonality Heatmap", "figure"),
         Output("Bitcoin S2F", "figure"),
         Output("Bitcoin Z-Score", "figure"),
-    ],
+        Output("Bitcoin MCPC", "figure"),
+    ],  # Assuming you want to keep this output as well
     [Input("update-data", "n_clicks")],
->>>>>>> 5de1578 (Add shared pre-push hook script and formatted files)
 )
 def update_graphs(n_clicks):
     if n_clicks > 0:
@@ -234,10 +215,7 @@ def update_graphs(n_clicks):
         cache.delete_memoized(get_cached_data, get_seasonality_heatmap_plot)
         cache.delete_memoized(get_cached_data, get_s2f_plot)
         cache.delete_memoized(get_cached_data, get_z_score_plot)
-<<<<<<< HEAD
         cache.delete_memoized(get_cached_data, get_mcpc_plot)
-=======
->>>>>>> fd64180 (Update dashboard.py)
 
     # Fetch and return the updated data for each plot
     return (
@@ -245,10 +223,7 @@ def update_graphs(n_clicks):
         get_cached_data(get_seasonality_heatmap_plot),
         get_cached_data(get_s2f_plot),
         get_cached_data(get_z_score_plot),
-<<<<<<< HEAD
-        get_cached_data(get_mcpc_plot)
-=======
->>>>>>> 5de1578 (Add shared pre-push hook script and formatted files)
+        get_cached_data(get_mcpc_plot),
     )
 
 
