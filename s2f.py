155a7eb1ec870df987e8a78e1fdf8b4e8145e63a
NoturBusiness <104364732+NoturBusiness@users.noreply.github.com>
2024-05-17 18:02:28 +0000

    Update s2f.py

--- s2f.py
+++ s2f.py
@@ -58,11 +58,10 @@ def get_s2f_plot(symbol, currency):
         title=symbol + "$ 1D vs Stock-to-Flow Modeled Price",
         title_x=0.5,
         xaxis=dict(title="Date"),
-        yaxis=dict(title="Price", type="log"),
         plot_bgcolor="rgba(17, 17, 17, 1)",
         paper_bgcolor="rgba(0, 0, 0, 0)",
         font=dict(color="white"),
-        yaxis=dict(showticklabels=True, showgrid=False)
+        yaxis=dict(showticklabels=True, showgrid=False, title="Price", type="log")
     )
 
     fig.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
