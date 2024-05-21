2c960a5cace40ee77f13381e83d99f156692e90a
NoturBusiness <104364732+NoturBusiness@users.noreply.github.com>
2024-05-21 13:54:50 +0000

    Update sharpe_ratio.py

--- sharpe_ratio.py
+++ sharpe_ratio.py
@@ -1,8 +1,7 @@
-
 import requests
 import datetime
 import pandas as pd
-import plotly.graph_objs as go
+import plotly.graph_objects as go
 from data.get_historical_data import get_historical_data
 
 # Function to calculate rolling Sharpe ratio
@@ -12,22 +11,21 @@ def calculate_rolling_sharpe_ratio(data, window):
     rolling_std = returns.rolling(window=window).std()
     sharpe_ratio = rolling_mean / rolling_std * (252 ** 0.5)  # Annualized Sharpe ratio
     return sharpe_ratio
-    
-def get_sharpe(symbol,currency):
+
+def get_sharpe_plot(symbol, currency):
     # Get crypto historical data
-    crypto_data_dict = get_all_time_historical_data(symbol, currency)
+    crypto_data = get_historical_data(symbol, currency)
     
-    # Convert dictionary to DataFrame
-    crypto_data = pd.DataFrame(list(crypto_data_dict.items()), columns=['Date', 'Close'])
-    crypto_data['Date'] = pd.to_datetime(crypto_data['Date'])
-    crypto_data.set_index('Date', inplace=True)
-    crypto_data.sort_index(inplace=True)
+    # Create a DataFrame with the Date as the index
+    data_df = pd.DataFrame(crypto_data, columns=['Close'])
+    data_df['Date'] = pd.to_datetime(data_df.index, utc=True).tz_convert(None)
+    data_df.set_index('Date', inplace=True)
     
     # Default window length
     default_window = 180
     
     # Calculate rolling Sharpe ratio with default window
-    rolling_sharpe_ratio = calculate_rolling_sharpe_ratio(crypto_data, default_window)
+    rolling_sharpe_ratio = calculate_rolling_sharpe_ratio(data_df, default_window)
     
     # Create Plotly figure
     fig = go.Figure()
@@ -36,54 +34,48 @@ def get_sharpe(symbol,currency):
     fig.add_trace(go.Scatter(x=rolling_sharpe_ratio.index, y=rolling_sharpe_ratio,
                              mode='lines',
                              name=f'Rolling {default_window // 30}m Sharpe Ratio',
-                             line=dict(color='orange', width=2.25)))  # Set line width to 2
+                             line=dict(color='orange', width=2.25)))  # Set line width to 2.25
     
-    # Function to update plot based on window length
-    def update_figure(window_length):
-        rolling_sharpe_ratio = calculate_rolling_sharpe_ratio(crypto_data, window_length)
-        return [go.Scatter(x=rolling_sharpe_ratio.index, y=rolling_sharpe_ratio,
-                           mode='lines', name=f'Rolling {window_length // 30}m Sharpe Ratio',
-                           line=dict(color='orange', width=2.25))]  # Set line width to 2
-    
-    # Define the buttons
-    buttons = [
-        dict(label='3 Months',
-             method='update',
-             args=[{'y': [calculate_rolling_sharpe_ratio(crypto_data, 60).values],
-                    'name': 'Rolling 3m Sharpe Ratio'}]),
-        dict(label='6 Months',
-             method='update',
-             args=[{'y': [calculate_rolling_sharpe_ratio(crypto_data, 180).values],
-                    'name': 'Rolling 6m Sharpe Ratio'}]),
-        dict(label='12 Months',
-             method='update',
-             args=[{'y': [calculate_rolling_sharpe_ratio(crypto_data, 360).values],
-                    'name': 'Rolling 12m Sharpe Ratio'}])
-    ]
-    
-    # Update layout with buttons and styling
+    # Update layout with buttons and aesthetics similar to get_rainbow
     fig.update_layout(
-        title='Rolling Sharpe Ratio',
-        title_font=dict(color='white'),
+        title=f'{symbol} Rolling Sharpe Ratio',
         xaxis_title='Date',
-        xaxis_title_font=dict(color='white'),
         yaxis_title='Sharpe Ratio',
-        yaxis_title_font=dict(color='white'),
-        plot_bgcolor='black',
-        paper_bgcolor='black',
+        plot_bgcolor='rgba(17, 17, 17, 1)',  # Black background color
+        paper_bgcolor='rgba(0, 0, 0, 0)',    # Transparent paper color
         font=dict(color='white'),
-        updatemenus=[dict(active=1, buttons=buttons, direction="down", pad={"r": 10, "t": 10})],
+        updatemenus=[dict(
+            active=1,
+            buttons=[
+                dict(label='3 Months',
+                     method='update',
+                     args=[{'y': [calculate_rolling_sharpe_ratio(data_df, 60).values],
+                            'name': 'Rolling 3m Sharpe Ratio'}]),
+                dict(label='6 Months',
+                     method='update',
+                     args=[{'y': [calculate_rolling_sharpe_ratio(data_df, 180).values],
+                            'name': 'Rolling 6m Sharpe Ratio'}]),
+                dict(label='12 Months',
+                     method='update',
+                     args=[{'y': [calculate_rolling_sharpe_ratio(data_df, 360).values],
+                            'name': 'Rolling 12m Sharpe Ratio'}])
+            ],
+            direction="down",
+            pad={"r": 10, "t": 10}
+        )],
         xaxis=dict(showgrid=False),  # Hide vertical grid lines
-        yaxis=dict(showgrid=False)   # Hide horizontal grid lines
+        yaxis=dict(showgrid=False),  # Hide horizontal grid lines
+        title_x=0.5
     )
 
-return fig
+    fig.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
+
+    return fig
 
 if __name__ == "__main__":
-    # Get all-time historical data from CryptoCompare API
-    symbol = "BTC"
+    # Example usage
+    symbol = "ETH"
     currency = "USD"
 
-    fig = get_sharpe(symbol, currency)
-
+    fig = get_sharpe_plot(symbol, currency)
     fig.show()
